ae0f87a4e93cf31606f8d9a8e33b730db2622a54
袁文广 <ywg@tuojie.com>
2015-04-21 20:10:24 +0800

    添加piggo

--- controllers/default.go
+++ controllers/default.go
@@ -6,13 +6,13 @@ import (
 
 import "fmt"
 import "io/ioutil"
-import "net/http"
+
+//import "net/http"
 
 //import "os"
 //import "flag"
 import cspb "protocol"
 import proto "code.google.com/p/goprotobuf/proto"
-import log "code.google.com/p/log4go"
 import cs_handle "module/cs_handle"
 
 //import "flag"
@@ -26,54 +26,45 @@ func (c *MainController) Get() {
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplNames = "index.tpl"
 }
+func (c *MainController) Post() {
+	fmt.Println("***********Post Start***********")
+	beego.Debug("***********Post Start***********")
+	data_req, _ := ioutil.ReadAll(c.Ctx.Input.Request.Body)
+	c.Ctx.Input.Request.Body.Close()
 
-func procHttpMsg(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() //解析参数
-	if r.Method == "GET" {
-		fmt.Fprintf(w, "{\"error\":\"please use post\"}")
-		log.Error("error: client is use get mode")
+	var pkg_list_req cspb.CSPkgList
+	if err := proto.Unmarshal(data_req, &pkg_list_req); err != nil {
+		beego.Error("main.procHttpMsg: unmarshal data_req to CSPkgList fail err:%v", err)
+		return
+	}
 
-	} else if r.Method == "POST" {
-		log.Info("RemoteAddr is :%s", r.RemoteAddr)
+	beego.Info("*******Start and req is %v******", pkg_list_req)
+	if len(pkg_list_req.GetPkgs()) == 0 {
+		beego.Error("req no pkg account c:%s,s:%s",
+			pkg_list_req.GetCAccount(),
+			pkg_list_req.GetSAccount())
+		return
+	}
 
-		data_req, _ := ioutil.ReadAll(r.Body)
-		r.Body.Close()
+	var pkg_list_res = cspb.CSPkgList{
+		CAccount: proto.String(pkg_list_req.GetCAccount()),
+		SAccount: proto.String(pkg_list_req.GetSAccount()),
+		Pkgs:     nil,
+		Seq:      proto.Int64(pkg_list_req.GetSeq()),
+		Uid:      proto.Int64(pkg_list_req.GetUid()),
+	}
 
-		var pkg_list_req cspb.CSPkgList
-		if err := proto.Unmarshal(data_req, &pkg_list_req); err != nil {
-			log.Error("main.procHttpMsg: unmarshal data_req to CSPkgList fail err:%v", err)
-			return
-		}
+	cs_handle.PkgListHandle(pkg_list_req, &pkg_list_res)
+	beego.Debug("Res is:%v", pkg_list_res)
 
-		log.Info("*******Start and req is %v******", pkg_list_req)
-		if len(pkg_list_req.GetPkgs()) == 0 {
-			log.Error("req no pkg account c:%s,s:%s",
-				pkg_list_req.GetCAccount(),
-				pkg_list_req.GetSAccount())
-			return
-		}
+	data_res, err := proto.Marshal(&pkg_list_res)
+	if err != nil {
+		beego.Error("marshal fail err:%v", err)
+		return
+	}
 
-		var pkg_list_res = cspb.CSPkgList{
-			CAccount: proto.String(pkg_list_req.GetCAccount()),
-			SAccount: proto.String(pkg_list_req.GetSAccount()),
-			Pkgs:     nil,
-			Seq:      proto.Int64(pkg_list_req.GetSeq()),
-			Uid:      proto.Int64(pkg_list_req.GetUid()),
-		}
-
-		cs_handle.PkgListHandle(pkg_list_req, &pkg_list_res)
-		log.Debug("Res is:%v", pkg_list_res)
-
-		data_res, err := proto.Marshal(&pkg_list_res)
-		if err != nil {
-			log.Error("marshal fail err:%v", err)
-			return
-		}
-
-		if _, err := w.Write(data_res); err != nil {
-			log.Error("write data fail err:%v", err)
-			return
-		}
-		log.Info("******End and res is %v******", pkg_list_res)
+	if _, err := c.Ctx.Output.Context.ResponseWriter.Write(data_res); err != nil {
+		beego.Error("write data fail err:%v", err)
+		return
 	}
 }
--- main.go
+++ main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"os"
 	_ "tuojie.com/piggo/quickstart.git/routers"
 )
 
-//import "fmt"
-//import "io/ioutil"
-//import "net/http"
-import "os"
-
-//import "flag"
-//import cspb "protocol"
-//import proto "code.google.com/p/goprotobuf/proto"
-import log "code.google.com/p/log4go"
 import cs_handle "module/cs_handle"
 import db_session "module/db/session"
 import db_collection "module/db/collection"
@@ -24,55 +16,49 @@ import config "config_data"
 func main() {
 	Init()
 	beego.Run()
-	finish()
+	Finish()
 }
 
 func Init() {
-
-	//log的初始化 log 初始化要放倒最前面，再log初始化之前无法打印log
-	log.LoadConfiguration("./conf/zoo_log.xml")
-	log.Debug("******Start main.Init******")
-	//命令行初始化
-	//flag.Parse()
+	beego.Debug("******Start main.Init******")
 
 	//msg handle 初始化
-	log.Debug("cs_handle Init")
+	beego.Debug("cs_handle Init")
 	cs_handle.Init()
 
-	log.Debug("config init")
+	beego.Debug("config init")
 	config.InitConfig()
 
 	//db  初始化
-	log.Debug("db init")
+	beego.Debug("db init")
 	err := db_session.Init(config.Config().GetDbIp())
 	//err := db_session.Init("127.0.0.1:12306")
 	if err != nil {
-		log.Error("Init db_session fail")
+		beego.Error("Init db_session fail")
 		os.Exit(1)
 	}
 
-	log.Debug("init user id")
+	beego.Debug("init user id")
 	db_collection.InitUserId()
 
 	//随机数初始化
-	log.Debug("init rand succ")
+	beego.Debug("init rand succ")
 	rand.Init()
 
 	//resource 初始化
-	log.Debug("init res_mgr succ")
+	beego.Debug("init res_mgr succ")
 	res_mgr.Init()
 
-	log.Debug("******End main.Init******")
+	beego.Debug("******End main.Init******")
 }
 
-func finish() {
-	log.Debug("******Start main.finish******")
+func Finish() {
+	beego.Debug("******Start main.finish******")
 
 	//DB 关闭
-	log.Info("db session close")
+	beego.Info("db session close")
 	db_session.Finish()
 
 	//log 关闭,注意顺序，log关闭后无法再打印log了。所以要保证放到最后
-	log.Info("log close")
-	log.Close()
+	beego.Info("log close")
 }
