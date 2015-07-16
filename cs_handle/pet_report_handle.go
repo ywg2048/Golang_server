d922c9691c5453f9b11b3ea22beb849939255ab1
袁文广 <ywg@tuojie.com>
2015-07-16 18:24:52 +0800

    更换小伙伴

--- cs_handle/pet_report_handle.go
+++ cs_handle/pet_report_handle.go
@@ -2,88 +2,39 @@ package cs_handle
 
 import (
 	"github.com/astaxie/beego"
+	models "tuojie.com/piggo/quickstart.git/models"
 )
 import cspb "protocol"
-import proto "code.google.com/p/goprotobuf/proto"
-import db "tuojie.com/piggo/quickstart.git/db/collection"
 
-import resmgr "tuojie.com/piggo/quickstart.git/res_mgr"
+// import proto "code.google.com/p/goprotobuf/proto"
+// import db "tuojie.com/piggo/quickstart.git/db/collection"
+import "labix.org/v2/mgo/bson"
+import db_session "tuojie.com/piggo/quickstart.git/db/session"
+
+// import resmgr "tuojie.com/piggo/quickstart.git/res_mgr"
 
 func petReportHandle(
 	req *cspb.CSPkg,
 	res_list *cspb.CSPkgList) int32 {
 
 	pet_id := req.GetBody().GetReportPetReq().GetPetId()
-	pet_db, ret_pet := db.GetPetById(res_list.GetSAccount(), pet_id)
-	if ret_pet < 0 {
-		beego.Error("get pet info error ret:%d", ret_pet)
-		return makePetReportResPkg(req, res_list,
-			int32(cspb.ErrorCode_PetIdIsInvalid))
+	ret := int32(1)
+	Ret := int32(0)
+	c := db_session.DB("zoo").C("player")
+	var player models.Player
+	c.Find(bson.M{"uid": int32(res_list.GetUid())}).One(&player)
+	_, err := c.Upsert(bson.M{"uid": int32(res_list.GetUid())},
+		bson.M{"$set": bson.M{"starid": pet_id}})
+	if err == nil {
+		Ret = int32(1)
+		beego.Info("明星更换成功！")
+	} else {
+		beego.Error("明星更换失败！")
 	}
 
-	if ret_pet == 0 {
-		var pet_list []*cspb.PetInfo
-
-		var cardNtf []*cspb.CSPetCardNtf
-		for i := range pet_db.Petcard {
-			cardNtf = append(cardNtf, makecardNtf(pet_db.Petcard[i].CardId, pet_db.Petcard[i].CardNum))
-		}
-
-		pet_list = append(pet_list, makePet(pet_db.PetId,
-			pet_db.PetLevel,
-			pet_db.PetCurExp,
-			pet_db.PetTotalExp,
-			pet_db.PetStarLevel,
-			pet_db.Petmedallevel,
-			pet_db.PetmedalNum,
-			pet_db.DressId,
-			cardNtf,
-		))
-
-		//添加pet_ntf到res_list中
-		makePetNtf(pet_list, res_list)
-
-		beego.Error(" pet is exist pet_id:%d, ret:%d", pet_id, ret_pet)
-		return makePetReportResPkg(req, res_list,
-			int32(cspb.ErrorCode_PetIdIsExist))
-	}
-
-	start_pet_star_level := resmgr.PetData.GetItems()[pet_id-1].GetStartStarLevel()
-	db.SetPetInfo(res_list.GetSAccount(),
-		pet_id, int32(1),
-		int32(0), int32(0), start_pet_star_level)
-
-	var pet_list []*cspb.PetInfo
-
-	var cardNtf []*cspb.CSPetCardNtf
-	for i := range pet_db.Petcard {
-		cardNtf = append(cardNtf, makecardNtf(pet_db.Petcard[i].CardId, pet_db.Petcard[i].CardNum))
-	}
-
-	pet_list = append(pet_list, makePet(pet_id,
-		int32(1),
-		int32(0),
-		int32(0),
-		start_pet_star_level,
-		int32(1),
-		int32(150),
-		int32(2),
-		cardNtf,
-	))
-
-	//添加pet_ntf到res_list中
-	makePetNtf(pet_list, res_list)
-
-	return makePetReportResPkg(req, res_list, int32(0))
-}
-
-func makePetReportResPkg(req *cspb.CSPkg,
-	res_list *cspb.CSPkgList, ret int32) int32 {
-
-	//填充petreportres回包
 	res_data := new(cspb.CSReportPetRes)
 	*res_data = cspb.CSReportPetRes{
-		Ret: proto.Int32(ret),
+		Ret: &Ret,
 	}
 
 	res_pkg_body := new(cspb.CSBody)
